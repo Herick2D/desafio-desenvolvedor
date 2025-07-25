f1c1388740411ab0025dde3d78086695853ed12a
Herick2D <contato.herickmoreira@gmail.com>
2025-07-25 17:07:05 -0300

    FEAT: validação de login

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,21 @@
 <?php
 
-use App\Http\Controllers\Api\InstrumentDataController;
-use App\Http\Controllers\Api\UploadController;
-use App\Http\Controllers\Api\UploadHistoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UploadController;
+use App\Http\Controllers\Api\UploadHistoryController;
+use App\Http\Controllers\Api\InstrumentDataController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::post('/upload', [UploadController::class, 'store']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/upload-history', [UploadHistoryController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::get('/data', [InstrumentDataController::class, 'index']);
+    Route::post('/upload', [UploadController::class, 'store']);
+    Route::get('/upload-history', [UploadHistoryController::class, 'index']);
+    Route::get('/data', [InstrumentDataController::class, 'index']);
+});
