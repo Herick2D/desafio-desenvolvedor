ebf7110a9451e324e185862f253119b8337a6653
Herick2D <contato.herickmoreira@gmail.com>
2025-07-26 16:37:11 -0300

    FIX: Docker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,22 +1,30 @@
-#!/bin/sh
-
+#!/usr/bin/env sh
 set -e
 
-if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-progress --no-interaction
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775      /var/www/html/storage /var/www/html/bootstrap/cache
+
+if [ ! -f /var/www/html/vendor/autoload.php ]; then
+  composer install --no-progress --no-interaction
 fi
+
+cd /var/www/html
 
 if [ ! -f ".env" ]; then
-    echo "Criando arquivo .env a partir do .env.example"
-    cp .env.example .env
+  cp .env.example .env
 fi
-
 if [ -z "$(grep -E '^APP_KEY=' .env | cut -d '=' -f2-)" ]; then
-    echo "Gerando chave da aplicação..."
-    php artisan key:generate
+  php artisan key:generate
 fi
 
-echo "Executando migrations..."
+php artisan config:clear
+
+echo "Esperando MySQL em mysql:3306..."
+while ! nc -z mysql 3306; do
+  sleep 1
+done
+echo "MySQL pronto."
+
 php artisan migrate --force
 
 exec "$@"
